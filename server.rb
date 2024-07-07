e76031213130fc15e5aa48aad50d5513d63e4496
ucode0101 <ucode0101@gmail.com>
2024-07-07 00:16:41 -0400

    changes

--- server.rb
+++ server.rb
@@ -575,9 +575,6 @@ class Public < Sinatra::Base
     erb :radio_buttons
   end
 
-  get '/radio_buttons2' do
-    erb :radio_buttons2
-  end
 
   get '/registration_form' do
     erb :registration_form
